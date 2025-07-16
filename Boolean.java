b7e54f3da7553af4ef2984e2de7230a07aa2871d
Soham-patra-kiit <136213022+Soham-patra-kiit@users.noreply.github.com>
2025-07-16 23:46:37 +0530

    First Commit

---
+++ Boolean.java
@@ -0,0 +1,13 @@
+package com.revature22;
+
+public class Boolean {
+    public static void main(String[] args)
+    {
+        boolean a = true;
+        boolean b = false;
+
+        System.out.println("!a = " + !a);
+        System.out.println("a | b = " + (a|b));
+        System.out.println("(!a & b) | (a & !b) = " + ((!a & b) | (a & !b)));
+    }
+}
